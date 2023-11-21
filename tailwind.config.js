5a9d1e639e337fa5a22ed27d0a620a4e98c7d1c3
PC1 <kevsalomdev@gmail.com>
2023-11-21 17:16:46 -0400

    Form practice with novak

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+           novak: "url('https://fotografias.lasexta.com/clipping/cmsimages01/2023/11/20/F302A6DD-D5D1-4973-8642-57CDFC3D6DF8/novak-djokovic_98.jpg?crop=3000,1688,x0,y0&width=1900&height=1069&optimize=low&format=webply')"
+      },
+      fontFamily:{
+        poppins: 'Poppins'
+      }
+    },
   },
   plugins: [],
 }
